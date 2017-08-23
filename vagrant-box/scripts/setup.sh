b6a9419977e6c36af2864e6c3e5f5e328aafd782
apcros <apcros.mail@gmail.com>
2017-08-23 13:25:28 +0100

    Fixed HTTPS, Permissions and Missing package

--- vagrant-box/scripts/setup.sh
+++ vagrant-box/scripts/setup.sh
@@ -3,13 +3,16 @@ echo "=== apt-get update ==="
 sudo apt-get update
 
 echo "=== Installing all required packages ==="
-sudo apt-get install apache2 php php-pgsql php-json postgresql php7.0-xml php-mbstring phpunit php-zip git -y
+sudo apt-get install apache2 php php-pgsql php-json postgresql php7.0-xml php-mbstring phpunit php-zip php-curl git -y
 
 echo "=== Configuring Apache2 ==="
-#TODO : HTTPS config
 sudo cp /vagrant/confs/apache_dev_inspicio.conf /etc/apache2/sites-enabled/dev_inspicio.conf
-#TODO : Change apache user to be vagrant
+sudo rm /etc/apache2/envvars
+sudo cp /vagrant/confs/apache_dev_inspicio.envvars /etc/apache2/envvars
+sudo chown -R ubuntu:ubuntu /var/www/
+sudo sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
 sudo a2enmod rewrite
+sudo a2enmod ssl
 sudo service apache2 restart
 
 echo "=== Configuring PostgreSQL ==="
@@ -18,7 +21,7 @@ sudo -u postgres psql -c "CREATE USER inspicio_dev;"
 sudo -u postgres psql -c "ALTER ROLE inspicio_dev WITH CREATEDB;"
 sudo -u postgres psql -c "CREATE DATABASE inspicio_db_dev OWNER inspicio_dev;"
 sudo -u postgres psql -c "ALTER USER inspicio_dev WITH ENCRYPTED PASSWORD 'inspicio_dev_password'"
-#sudo sed -i 's/peer/md5/' /etc/postgresql/9.3/main/pg_hba.conf
+sudo sed -i 's/peer/md5/' /etc/postgresql/*/main/pg_hba.conf
 sudo /etc/init.d/postgresql restart
 
 echo "=== Installing Composer ==="
@@ -29,11 +32,12 @@ php -r "unlink('composer-setup.php');"
 
 echo "=== Setting up Inspicio ==="
 cd /var/www/html/ && git clone https://github.com/apcros/Inspicio.git
+sudo chown -R ubuntu:ubuntu /var/www/
 cd /var/www/html/Inspicio
 cp /vagrant/confs/env_inspicio_dev.env .env
 composer install
 php artisan key:generate
-#TODO : Start the queue ?
-#TODO : Copy the env script
+php artisan migrate
 
 sudo ln -s /var/www/html/ /vagrant/vm-www-dir
+sudo service apache2 start
